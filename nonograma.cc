1b34846aa802040a09cf59072c33f980381fa9f5
Titider1419 <andres.faccia@estudiantat.upc.edu>
2024-12-03 10:29:26 +0100

    Add files via upload

--- nonograma.cc
+++ nonograma.cc
@@ -6,41 +6,43 @@ typedef vector< vector<char> > Matriu;
 typedef vector< vector<int> > Matriuent;
 
 void mostrar_tauler(Matriu &mat,unsigned int &fil, unsigned int &col, Matriuent &hor, Matriuent &ver, unsigned int &m){
-	cout << " ";
+	cout << "  ";
 	for(unsigned int i = 0; i<col; ++i){
 		cout << "  " << i+1;
 	}
+	cout << " ";
 	cout << endl;
-	cout << "   ";
+	cout << "    ";
 	for(unsigned int i = 0; i<col; ++i){
 		if (i+1<col) cout << "---";
 		else cout << "--";
 	}
 	cout << endl;
 	for(unsigned int i = 0; i<fil; ++i){
-		if (i+1<10) cout << i+1 << " |";
-		else cout << i+1 << "|";
+		if (i+1<10) cout << " " << i+1 << " |";
+		else cout << i+1 << " |";
 		for(unsigned int j=0; j<col; ++j){
 			if(j+1<col) cout << mat[i][j] << "  ";
 			else cout << mat[i][j];
 		}
 		cout << " |";
 		for(unsigned int j=0; j<hor[i].size(); ++j){
-			if (j+1 < hor[i].size()) cout << hor[i][j] << " ";
+			if (j+1 < hor[i].size()) cout << " " << hor[i][j];
 			else cout << hor[i][j];
 		}
 		cout << endl;
 	}
-	cout << "   ";
+	cout << "    ";
 	for(unsigned int i = 0; i<col; ++i){
 		if (i+1<col) cout << "---";
 		else cout << "--";
 	}
 	cout << endl;
 	for(unsigned int j = 0; j<m; ++j){
-		cout << " ";
-		for(unsigned int i = 0; i<col; ++i){
-			if (ver[i][j] != 0) cout << "  " << ver[i][j];
+		cout << "  ";
+		for(unsigned int i = 0; i<col;
+++i){
+			if (j < ver[i].size()) cout << "  " << ver[i][j];
 			else cout << "   ";
 		}
 		cout << endl;
@@ -61,7 +63,7 @@ int main(){
 			pistes_hor[i].push_back(m);
 		}
 	}
-	Matriuent pistes_ver(files, vector<int>());
+	Matriuent pistes_ver(columnes, vector<int>());
 	unsigned int max = 0;
 	for(unsigned int i=0; i<columnes; ++i){
 		unsigned int n, m;
@@ -72,6 +74,21 @@ int main(){
 			pistes_ver[i].push_back(m);
 		}
 	}
-	cout << pistes_ver.size() << endl;
 	mostrar_tauler(tauler, files, columnes, pistes_hor, pistes_ver, max);
+	bool Z = false, resolt = false;
+	while(not resolt and not Z){
+		char operacio;
+		cin >> operacio;
+		unsigned int fil1, col1;
+		if (operacio == 'A'){
+			cin >> fil1 >> col1;
+			tauler[fil1-1][col1-1] = 'X';
+		}
+		else if (operacio == 'E'){
+			cin >> fil1 >> col1;
+			tauler[fil1-1][col1-1] = '.';
+		}
+		else if (operacio == 'S') mostrar_tauler(tauler, files, columnes, pistes_hor, pistes_ver, max);
+		else if (operacio == 'Z') Z = true;
+	}
 }
